5dfe2a0b23a67d9e36edf54b47f7db8b947c5fd0
viktormuzyka <42584540+viktormuzyka@users.noreply.github.com>
2022-06-19 21:20:34 +0300

    Delete src/sem2/lab_1(uml)/uml_documentation directory

--- src/sem2/lab_1(uml)/uml_documentation/class_node.js
+++
@@ -1,7 +0,0 @@
-var class_node =
-[
-    [ "Node", "class_node.html#ae08e0f91cf2e56629685a36bd7272d9a", null ],
-    [ "data", "class_node.html#ac450c71a8677a38d306361f9ced518d3", null ],
-    [ "next", "class_node.html#ac1c0563946c59c36bddde431b4adb00b", null ],
-    [ "pos", "class_node.html#a0e38cc6840bd2dcd42706f7bdef539e6", null ]
-];
